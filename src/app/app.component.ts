6c774adad607b5a86c9247b8c60a7215c556457f
shybzzz <rostykrom@yahoo.com>
2018-12-16 06:49:41 +0200

    mqtt and google charts are introduced

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MqttService } from 'ngx-mqtt';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'well-sensor-filtering-ui';
+  d = [
+    ['London', 8136000],
+    ['New York', 8538000],
+    ['Paris', 2244000],
+    ['Berlin', 3470000],
+    ['Kairo', 19500000]
+  ];
+
+  constructor(private mqtt: MqttService) {}
+
+  ngOnInit() {
+    const mqtt = this.mqtt;
+    mqtt.connect({
+      username: 'gelphadi',
+      password: 'oYMgWJETz_0N',
+      servers: [{ host: 'perfect-politician.cloudmqtt.com', port: 443 }]
+    });
+
+    mqtt.observe('Rostyk/Data').subscribe(r => console.log(r));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { GoogleChartsModule } from 'angular-google-charts';
+import { MqttModule } from 'ngx-mqtt';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GoogleChartsModule.forRoot(),
+    MqttModule.forRoot({ connectOnCreate: false, path: '', protocol: 'wss' })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
